9ab6f7357e1d9f1889f41073f0ed56289ee68b44
7x <info@se7enx.com>
2025-09-28 03:52:46 -0700

    Add missing configuration parsers for Content and User Settings views
    Added the following configuration parsers that were missing:
    - ContentEditView.php
    - ContentCreateView.php
    - UserSettingsUpdateView.php
    - SearchView.php
    
    These parsers are required for proper configuration handling
    in EZ Platform installations and prevent 'class not found' errors
    when loading the dependency injection container.

---
+++ eZ/Bundle/EzPublishCoreBundle/DependencyInjection/Configuration/Parser/ContentCreateView.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\Parser;
+
+class ContentCreateView extends View
+{
+    public const NODE_KEY = 'content_create_view';
+    public const INFO = 'Template selection settings when creating content';
+}
